0f190e61ce82798c163953a4c8ac1de553194274
Eburnfelix93 <eburnfelixx@gmail.com>
2022-06-03 02:07:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -18,13 +18,9 @@
  */
 
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
 
 
