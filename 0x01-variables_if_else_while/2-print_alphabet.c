062f803fab7b96eaeeb18d86846f323451406eb2
Eburnfelix93 <eburnfelixx@gmail.com>
2022-06-05 02:12:33 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	char alpha;
+char alpha;
 
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-		putchar(alpha);
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+putchar(alpha);
 
-	putchar('\n');
+putchar('\n');
 
-	getalpha();
+getalpha();
 }
