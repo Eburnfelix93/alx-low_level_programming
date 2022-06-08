1082b09d96b5640da1a98aebe71528f22d11a071
Eburnfelix93 <eburnfelixx@gmail.com>
2022-06-08 04:16:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,7 @@
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
+	putchar('_putchar');
 	putchar('\n');
 
 	return (0);
