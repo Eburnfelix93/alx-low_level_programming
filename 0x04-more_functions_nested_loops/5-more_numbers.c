cf32dec336fb7637013e167302f0bbb76ae4b03d
Eburnfelix93 <eburnfelixx@gmail.com>
2022-06-10 08:58:02 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,13 @@ void more_numbers(void)
 {
 	int c, i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			_putchar(c);
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
