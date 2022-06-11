fc9b61739e13729bf90b1d44c992492379bb16f5
Eburnfelix93 <eburnfelixx@gmail.com>
2022-06-11 01:29:28 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * print_numbers - Prints the numbers from 0-9.
+ * print_numbers - print  0 - 9
+ * Return: void
  */
 void print_numbers(void)
 {
-int c;
-
-for (c = '0'; c <= '9'; c++)
-
-_putchar((num % 10) + '0');
-
-
+char a = 0;
+while (a <= 9)
+{
+_putchar(a + '0');
+a++;
+}
 _putchar('\n');
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Write a script that runs a C file through the preprocessor and save the result into another file.
+ tests function that prints if integer is positive or negative
