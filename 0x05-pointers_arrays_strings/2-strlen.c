f3009047f8e722586e6e971e2666bd0a3dc5437b
Eburnfelix93 <eburnfelixx@gmail.com>
2022-06-14 02:10:12 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: String to count
+ *
+ * Return:String length
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i]; != '\0'; ++i)
+return (c);
+}
