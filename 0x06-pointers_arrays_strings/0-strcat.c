8a46121f7a9edafaa059ad6c0e4783f8ebec4b23
Eburnfelix93 <eburnfelixx@gmail.com>
2022-06-16 11:37:08 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int d = 0;
+	int d = 0, i;
 
-	while (dest[i++])
-
-		d++;
-
-	for (i = 0; src[i]; i++)
-
-		dest[d++] = src[i];
-
-	return (dest);
+while (dest[d])
+{
+d++;
+}
+for (i = 0; src[i] != 0; i++)
+{
+dest[d] = src[i];
+d++;
+}
+dest[d] = '\0';
+return (dest);
 }
