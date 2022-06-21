cc0e40a68f4779c7337a9426cb3d9097df81440c
Eburnfelix93 <eburnfelixx@gmail.com>
2022-06-20 21:32:58 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _strpbrk - search a string for any of a set of bytes
+ * @s: source string
+ * @accept: accepted characters
+ *
+ * Return: the string since the first found accepted character
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, x;
+
+	while (s[i])
+	{
+		x = 0;
+		while (accept[x])
+		{
+			if (s[i] == accept[x])
+			{
+				s += i;
+				return (s);
+			}
+			x++;
+		}
+		i++;
+	}
+	return ('\0');
+}
